d069825b22e29880d9afef1db3a4b87994bb328b
Jafreedman Moffat <jahsfreedman12@gmail.com>
2025-09-09 22:36:58 +0000

    remove the scrollbar in the sidebar

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -417,7 +417,7 @@ const SidebarContent = React.forwardRef<
       ref={ref}
       data-sidebar="content"
       className={cn(
-        "flex min-h-0 flex-1 flex-col gap-2 overflow-y-auto overflow-x-hidden group-hover:overflow-auto",
+        "flex min-h-0 flex-1 flex-col gap-2 overflow-hidden",
         className
       )}
       {...props}
